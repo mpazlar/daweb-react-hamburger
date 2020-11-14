0af60b44db9da3963db24387ae8b268827d7fcd3
Martin Podloucký <podlouckymartin@gmail.com>
2020-11-14 08:44:54 +0100

    Základní kód pro cvičení.

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './style.css';
 import './index.html';
 
-render(
-  <>
+const MenuItem = (props) => {
+  return (
+    <a href="#" className="menu-item">
+      {props.text}
+    </a>
+  );
+};
+
+const App = () => {
+  return (
     <header>
-      <h1>React Starter</h1>
+      <div className="menu">
+        <button className="menu__btn"></button>
+        <div className="menu__items">
+          <MenuItem text="Domů" />
+          <MenuItem text="Naše nabídka" />
+          <MenuItem text="Náš tým" />
+          <MenuItem text="Blog" />
+          <MenuItem text="Kontakt" />
+        </div>
+      </div>
     </header>
-    <main>
-      <p>Moje první React stránka</p>
-    </main>
-    <footer>Martin Podloucký</footer>
-  </>,
-  document.querySelector('#app'),
-);
+  );
+};
+
+render(<App />, document.querySelector('#app'));
